50801a15ce7b1f5ea2b9828d9fee37bf21473e61
akarawatp <akarawat.p@siamkubota.co.th>
2023-05-19 10:55:29 +0700

    test query

--- Notify_Engine_Location/Location_notify.py
+++ Notify_Engine_Location/Location_notify.py
@@ -12,73 +12,74 @@ import numpy as np
 import urllib
 from . import shapegeocode
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, ContentSettings
+from io import StringIO
+import logging
 
 start = datetime.datetime.today()
 # geopathc = r"C:\Users\akarawat.p\Desktop\Data for Bridge\KIS\world_countries_generalized"
 # gc = shapegeocode.geocoder(os.path.join(geopathc,'World_Countries__Generalized_.shp'))
 
-# blob_client = BlobClient.from_blob_url("https://kisnewstorage.blob.core.windows.net/worldcountriesgeneralized/World_Countries__Generalized_.shp?sp=r&st=2023-05-19T02:00:03Z&se=2023-12-31T10:00:03Z&spr=https&sv=2022-11-02&sr=b&sig=aPvt6ij3F5n8WNDqW%2BQUEM%2FNNs8H4UNUkX%2FdrXFUp28%3D")
-# with open(file=os.path.join('world_countries', 'World_Countries__Generalized_.shp'), mode="wb") as sample_blob:
-#     download_stream = blob_client.download_blob()
-#     sample_blob.write(download_stream.readall())
-def run():
-    gc = shapegeocode.geocoder(os.path.join('Notify_Engine_Location/world_countries', 'World_Countries__Generalized_.shp'))
-    print(gc)
 # geopath = r"D:\Data for Bridge\KIS\tha_adm_rtsd_itos_20220121_SHP_PART_2"
 # thgc = shapegeocode.geocoder(os.path.join(geopath,"tha_admbnda_adm3_rtsd_20220121.shp"))
+
+gc = shapegeocode.geocoder(os.path.join('Notify_Engine_Location/world_countries', 'World_Countries__Generalized_.shp'))
+thgc = shapegeocode.geocoder(os.path.join('Notify_Engine_Location/tha_adm_rtsd_itos',"tha_admbnda_adm3_rtsd_20220121.shp"))
 
 LineToken = 'upoFwEJRyecKFCBfouLgH0ugnCz3QppFaecAsSsca2M'
 LineToken2 = 'pTfbjW6EG1oWMT7rY0N3v50dqRzg038xjSLbHXF9C4y'
 
-# def find_co(thgc,gc,flt,fln,co):
-#     try:
-#         loc = thgc.geocode(flt, fln,max_dist=0.2)
-#         gcc = loc['ADM0_EN']
-#     except:
-#         try:
-#             loc2 = gc.geocode(flt,fln, max_dist = 0.2)
-#             gcc = loc2['COUNTRY']
-#         except:
-#             try:
-#                 loc2 = gc.geocode(flt,fln, max_dist = 0.2)
-#                 gcc = loc2['COUNTRYAFF']
-#             except:
-#                 print('cannot find')
-#                 gcc = co
-#     return gcc
+def find_co(thgc,gc,flt,fln,co):
+    try:
+        loc = thgc.geocode(flt, fln,max_dist=0.2)
+        gcc = loc['ADM0_EN']
+    except:
+        try:
+            loc2 = gc.geocode(flt,fln, max_dist = 0.2)
+            gcc = loc2['COUNTRY']
+        except:
+            try:
+                loc2 = gc.geocode(flt,fln, max_dist = 0.2)
+                gcc = loc2['COUNTRYAFF']
+            except:
+                logging.info('cannot find')
+                gcc = co
+    return gcc
 
-# def func_LineNotify(Message,LineToken):
-#     url  = "https://notify-api.line.me/api/notify"
-#     msn = {'message':Message}
-#     LINE_HEADERS = {"Authorization":"Bearer " + LineToken}
-#     session  = requests.Session()
-#     response =session.post(url, headers=LINE_HEADERS, data=msn)
-#     return response 
+def func_LineNotify(Message,LineToken):
+    url  = "https://notify-api.line.me/api/notify"
+    msn = {'message':Message}
+    LINE_HEADERS = {"Authorization":"Bearer " + LineToken}
+    session  = requests.Session()
+    response =session.post(url, headers=LINE_HEADERS, data=msn)
+    return response 
 
-# if __name__ == '__main__':
-#     file = pd.read_csv(r'D:\Data for Bridge\KIS\API_Record\check_abroad.csv')
-#     compare = pd.DataFrame(file,columns=['name'])
-#     old_name = compare['name'].tolist()
+def run():
+    blob_client = BlobClient.from_blob_url("https://kisnewstorage.blob.core.windows.net/apirecord/check_abroad.csv?sp=raw&st=2023-05-19T03:35:34Z&se=2023-12-31T11:35:34Z&spr=https&sv=2022-11-02&sr=b&sig=osOnR1YyokhysQwvDYVQFs3c7w5OmcT%2Fnw3YY7VZKRg%3D")
+    download_stream = blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
+    file = pd.read_csv(StringIO(download_stream.readall()), low_memory=False)
+    # file = pd.read_csv(r'D:\Data for Bridge\KIS\API_Record\check_abroad.csv')
+    compare = pd.DataFrame(file,columns=['name'])
+    old_name = compare['name'].tolist()
 
-#     server = 'tcp:consentdb.database.windows.net'
-#     database = 'kis-hour-test'
-#     username = 'consent-user'
-#     password = 'P@ssc0de123'
-#     driver = '{ODBC Driver 17 for SQL Server}'  
-#     dsn = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password
+    server = 'tcp:consentdb.database.windows.net'
+    database = 'kis-hour-test'
+    username = 'consent-user'
+    password = 'P@ssc0de123'
+    driver = '{ODBC Driver 17 for SQL Server}'  
+    dsn = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+password
 
-#     mydb = pyodbc.connect(dsn)
-#     result = pd.read_sql_query('''select positionTime,equipmentName,latitude,longitude,Country from (select * from Engine_location_record as b inner join 
-#     (select equipmentName as certain_name, max(positionTime) as latest from Engine_location_record group by equipmentName) as a 
-#      on b.equipmentName = a.certain_name and b.positionTime = latest where ADM3_PCODE is null) as c''',mydb)
+    mydb = pyodbc.connect(dsn)
+    result = pd.read_sql_query('''select positionTime,equipmentName,latitude,longitude,Country from (select * from Engine_location_record as b inner join 
+    (select equipmentName as certain_name, max(positionTime) as latest from Engine_location_record group by equipmentName) as a 
+     on b.equipmentName = a.certain_name and b.positionTime = latest where ADM3_PCODE is null) as c''',mydb)
 
-#     detail_db = pymssql.connect(server='DEV01SVR', user='boon', password='tryTh1$@h0me', database='KisRecord')
-#     skl = pd.read_sql_query('''(select Equipment_Name as equipmentName, SKL from Engine_Detail where SKL = '1')''',detail_db)
+    detail_db = pymssql.connect(server='skcdwhprdmi.siamkubota.co.th', user='skcadminuser', password='DEE@skcdwhtocloud2022prd', database='KIS Data')
+    skl = pd.read_sql_query('''(select Equipment_Name as equipmentName, SKL from Engine_Detail where SKL = '1')''',detail_db)
 
-#     thismess = result.merge(skl, how='inner', on='equipmentName')
-#     thismess.drop(columns = 'SKL')
+    thismess = result.merge(skl, how='inner', on='equipmentName')
+    thismess.drop(columns = 'SKL')
 
-#     print("Finish query")
+    logging.info("Finish query")
 
 #     l_time = []
 #     l_eq = []
